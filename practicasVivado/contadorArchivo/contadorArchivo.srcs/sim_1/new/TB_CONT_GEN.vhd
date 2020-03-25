bde9dc4cd9dd945b5284e0bdb947c73119c04990
Adrian González Pardo <gozapaadr@gmail.com>
2020-03-24 18:23:22 -0600

    Sim con archivos

---
+++ practicasVivado/contadorArchivo/contadorArchivo.srcs/sim_1/new/TB_CONT_GEN.vhd
@@ -0,0 +1,107 @@
+LIBRARY ieee;
+LIBRARY STD;
+USE STD.TEXTIO.ALL;
+USE ieee.std_logic_TEXTIO.ALL;	--PERMITE USAR STD_LOGIC 
+
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_UNSIGNED.ALL;
+USE ieee.std_logic_ARITH.ALL;
+ 
+ENTITY TB_CONT_GEN IS
+END TB_CONT_GEN;
+
+ 
+ARCHITECTURE behavior OF TB_CONT_GEN IS  
+    -- Component Declaration for the Unit Under Test (UUT)
+    COMPONENT CONT_GEN
+    PORT(
+         CLK : IN  std_logic;
+         CLR : IN  std_logic;
+         D : IN  std_logic_vector(7 downto 0);
+         OPER : IN  std_logic_vector(1 downto 0);
+         Q : INOUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+   --Inputs
+   signal CLK : std_logic := '0';
+   signal CLR : std_logic := '0';
+   signal D : std_logic_vector(7 downto 0) := (others => '0');
+   signal OPER : std_logic_vector(1 downto 0) := (others => '0');
+	--BiDirs
+   signal Q : std_logic_vector(7 downto 0);
+   -- Clock period definitions
+   constant CLK_period : time := 10 ns;
+BEGIN
+	-- Instantiate the Unit Under Test (UUT)
+   uut: CONT_GEN PORT MAP (
+          CLK => CLK,
+          CLR => CLR,
+          D => D,
+          OPER => OPER,
+          Q => Q
+        );
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+   -- Stimulus process																	
+   stim_proc: process
+	file ARCH_RES : TEXT;																					
+	variable LINEA_RES : line;
+	VARIABLE VAR_Q : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	
+	file ARCH_VEC : TEXT;
+	variable LINEA_VEC : line;
+	VARIABLE VAR_OPER : STD_LOGIC_VECTOR(1 DOWNTO 0);
+	VARIABLE VAR_D : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	VARIABLE VAR_CLR : STD_LOGIC;
+	VARIABLE CADENA : STRING(1 TO 4);
+   begin		
+   
+        --Cambiar la ruta de donde esta su proyecto para la lectura del archivo
+		file_open(ARCH_VEC, "/media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/registro/VECTORES.TXT", READ_MODE); 	
+		file_open(ARCH_RES, "/media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/registro/RESULTADO.TXT", WRITE_MODE); 	
+
+		CADENA := "OPER";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);	--ESCRIBE LA CADENA "OPER"
+		CADENA := "   D";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);	--ESCRIBE LA CADENA "   D"
+		CADENA := " CLR";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);	--ESCRIBE LA CADENA " CLR"
+		CADENA := "   Q";
+		write(LINEA_RES, CADENA, right, CADENA'LENGTH+1);	--ESCRIBE LA CADENA "   Q"
+		writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+
+		WAIT FOR 100 NS;
+		FOR I IN 0 TO 14 LOOP
+			readline(ARCH_VEC,LINEA_VEC); -- lee una linea completa
+
+			read(LINEA_VEC, VAR_OPER);
+			OPER <= VAR_OPER;
+			Hread(LINEA_VEC, VAR_D);
+			D <= VAR_D;		
+			read(LINEA_VEC, VAR_CLR);  
+			CLR <= VAR_CLR;
+			
+			WAIT UNTIL RISING_EDGE(CLK);	--ESPERO AL FLANCO DE SUBIDA 
+
+			VAR_Q := Q;	
+			write(LINEA_RES, VAR_OPER, right, 5);	--ESCRIBE EL CAMPO OPER
+			Hwrite(LINEA_RES, VAR_D, 	right, 5);	--ESCRIBE EL CAMPO D
+			write(LINEA_RES, VAR_CLR, 	right, 5);	--ESCRIBE EL CAMPO CLR
+			Hwrite(LINEA_RES, VAR_Q, 	right, 5);	--ESCRIBE EL CAMPO Q
+
+			writeline(ARCH_RES,LINEA_RES);-- escribe la linea en el archivo
+			
+		end loop;
+		file_close(ARCH_VEC);  -- cierra el archivo
+		file_close(ARCH_RES);  -- cierra el archivo
+
+      wait;
+   end process;
+	
+END;
