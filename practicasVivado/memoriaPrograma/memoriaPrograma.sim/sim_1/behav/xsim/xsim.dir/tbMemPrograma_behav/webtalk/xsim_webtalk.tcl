132aacc59343af04b1eed7a55c751754afecfaf8
Adrian González Pardo <gozapaadr@gmail.com>
2020-04-08 19:56:45 -0500

    Practica 8 memoria de programa

--- practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/xsim_webtalk.tcl
+++ practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed Apr  8 19:46:02 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed Apr  8 19:56:20 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,19 +14,19 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "345387df-bd6e-4f35-b90b-84e62f311f93" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "43c36e4ddc264616b9001301cfc5f78b" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "5" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 18.04.4 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-3210M CPU @ 2.50GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "1292.322 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "1398.474 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "12.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "710 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "133948_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 4214243124 -regid "" -xml /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/usage_statistics_ext_xsim.xml -html /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/usage_statistics_ext_xsim.html -wdm /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 4182731501 -regid "" -xml /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/usage_statistics_ext_xsim.xml -html /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/usage_statistics_ext_xsim.html -wdm /media/d3vcr4ck/externData/materias-Sem20_2/arquitecturaDeComputadoras/arquitecturaDeComputadoras/practicasVivado/memoriaPrograma/memoriaPrograma.sim/sim_1/behav/xsim/xsim.dir/tbMemPrograma_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
