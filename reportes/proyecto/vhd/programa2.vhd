43d47ce5ca62945e4b3dfbf2bec0783d0d1dd436
Adrian González Pardo <gozapaadr@gmail.com>
2020-07-03 14:04:08 -0500

    Proyecto parte 1 del procesador ESCOMips de la serie de Fibonacci

--- reportes/proyecto/vhd/programa2.vhd
+++ reportes/proyecto/vhd/programa2.vhd
@@ -10,7 +10,7 @@ constant aux:banco:=(
     SUBI & R0 & R1 & x"000" ,           -- 5 SUBI R0,R1,#0
     SUBI & R1 & R4 & x"000" ,           -- 6 SUBI R1,R4,#0
     ADDI & R2 & R2 & x"001" ,           -- 7 ADDI R2,R2,#1
-    BLTI & R2 & R3 & x"FFC" ,           -- 8 BLTI R3,R2,-4
+    BLTI & R3 & R2 & x"FFC" ,           -- 8 BLTI R3,R2,-4
     NOP & SU & SU & SU & SU & SU ,      -- 9 NOP
     B & SU & x"0009" ,                  --10 B 9
     others=>(others=>'0')
